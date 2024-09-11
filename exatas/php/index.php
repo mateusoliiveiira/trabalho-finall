f475f12ae80f62bd7ecfd8d814212bd5bbfc7979
Mateus <mateusoliiveira000@gmail.com>
2024-09-11 11:22:39 -0300

    pastas

---
+++ exatas/php/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/index.css">
+    <title>Document</title>
+</head>
+<body>
+
+<!---------Começo------------>
+<header>
+.
+</header>
+<!---------------->
+
+
+<!---------MeiO-------------->
+<main>
+.
+</main>
+<!---------------->
+
+<!---------Fim--------------->
+<footer>
+
+.
+</footer>
+<!------------------>
+</body>
+</html>
