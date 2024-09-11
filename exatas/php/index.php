c87a4b741e520e744ee111c08c5193d7abcc29cc
Mateus <mateusoliiveira000@gmail.com>
2024-09-11 12:49:34 -0300

    comecando

--- exatas/php/index.php
+++ exatas/php/index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/index.css">
     <title>Document</title>
 </head>
@@ -10,15 +11,36 @@
 
 <!---------Começo------------>
 <header>
-.
+<h1>Materia Em Ação</h1>
 </header>
 <!---------------->
 
 
 <!---------MeiO-------------->
 <main>
-.
+
+<div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="banner" src="../img/banner2 (2).png" alt="Banner 1">
+      </div>
+      <div class="carousel-item">
+        <img class="banner" src="../img/banner2.png" alt="Banner 2">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </main>
+
 <!---------------->
 
 <!---------Fim--------------->
