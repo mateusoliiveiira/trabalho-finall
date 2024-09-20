23fb73d33f400c2deeeb53ff400d33d5b596bf75
Davi <rdavisamuel@gmail.com>
2024-09-20 13:58:42 -0300

    arrumei os header

--- exatas/php/cadastrar-matematica.php
+++ exatas/php/cadastrar-matematica.php
@@ -56,11 +56,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['nome'], $_POST['oquee
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Termos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/cdtermo.css"> 
     <link rel="stylesheet" href="../css/header.css"> 
 </head>
 <body>
-<header>  <h1>Adicionar Termo</h1>
+<header>  <h1 class="titulo">Adicionar Termo</h1>
 </header>  
     <?php if (isset($mensagem)) { ?>
         <p><?php echo htmlspecialchars($mensagem); ?></p>
--- exatas/php/cadastrar-materia.php
+++ exatas/php/cadastrar-materia.php
@@ -49,12 +49,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['materia'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nova Matéria</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/criarmateria.css">
     <link rel="stylesheet" href="../css/header.css">
 </head>
 <body>
     <header>
-        <h1>.</h1>
+        <h1 class="titulo">Adicionar Matéria</h1>
     </header>
     <div class="container-admateria">
         <div class="form-box-admateria">
