a724639736f4ea4f093617d79e747e9ac4f625e7
Davi <rdavisamuel@gmail.com>
2024-09-27 11:43:37 -0300

    alguns ajustes

--- exatas/php/listar_materias.php
+++ exatas/php/listar_materias.php
@@ -20,19 +20,17 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Lista de Matérias</title>
     <link rel="stylesheet" href="../css/listar.css">
 </head>
 <body>
+    <div class="header">
+        <h1 class="titulo2">Aprende facil</h1>
+    </div>
     <div class="container">
         <h1 class="titulo">Matérias</h1>
         <table>
-            <thead>
-                <tr>
-                    <th>Nome da matéria</th>
-                    <th>Delete</th>
-                </tr>
-            </thead>
             <tbody>
                 <?php
                 // Consulta para buscar todas as matérias
